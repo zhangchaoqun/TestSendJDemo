4e61f85844a6dbe7bf88518c7c39419c2346ae74
zhangchaoqun <1358649669@qq.com>
2017-09-05 15:28:55 +0800

    test demo

---
+++ testsendjlibrary/src/main/java/com/example/testsendjlibrary/LTest.java
@@ -0,0 +1,19 @@
+package com.example.testsendjlibrary;
+
+import android.util.Log;
+
+/**
+ * @discription:
+ * @author: zcq
+ * @e_mail: 2473117666@qq.com
+ * @date: 2017/9/5
+ * @version:
+ * @修改人：
+ * @修改记录:
+ */
+public class LTest {
+
+    public void testLog(String lMsg){
+        Log.e("测试打印log：",lMsg);
+    }
+}
